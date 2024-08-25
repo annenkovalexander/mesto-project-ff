9a6166a4b03f836d7db40dec1ec1437daa8ead7e
Alexander Annenkov <alexander@iMac-Alexander.local>
2024-08-25 10:12:15 +0300

    change delete handler

--- scripts/index.js
+++ scripts/index.js
@@ -10,28 +10,18 @@ const cardCreate = (card, deleteHandler) => {
     const cardTemplate = document.querySelector('#card-template').content;
     const cardPlacesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
     const cardImage = cardPlacesItem.querySelector('.card__image');
-    const cardButton = cardPlacesItem.querySelector('.card__delete-button');
+    const cardDeleteButton = cardPlacesItem.querySelector('.card__delete-button');
     const cardTitle = cardPlacesItem.querySelector('.card__title');
-    cardButton.onclick = () => deleteHandler(card.name);
+    cardDeleteButton.addEventListener('click', () => deleteHandler(cardPlacesItem));
     cardImage.src = card.link || "";
-    cardImage.alt = card.name || "";
+    cardImage.alt = "Mesto " + card.name || "";
     cardTitle.textContent = card.name || "";
     return cardPlacesItem;
 }
 
 // @todo: Функция удаления карточки
 
-const deleteHandler = (item) => {
-    const cardsList = document.querySelector('.places__list').children;
-    for (let i = 0; i<cardsList.length; i++){
-        const cardElement = cardsList[i];
-        const cardImage = cardElement.querySelector('.card__image');
-        if (cardImage.alt === item){
-            cardElement.remove();
-            break;
-        }
-    }
-}
+const deleteHandler = card => card.remove();
 
 // @todo: Вывести карточки на страницу
 const placesList = document.querySelector('.places__list');
